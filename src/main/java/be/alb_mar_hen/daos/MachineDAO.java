27cda424c6ae98addb1695e1ebe97c92dc64023d
GaryScript <gary.albrecq@condorcet.be>
2024-12-26 19:34:24 +0100

    findAllMethod in DAO

--- src/main/java/be/alb_mar_hen/daos/MachineDAO.java
+++ src/main/java/be/alb_mar_hen/daos/MachineDAO.java
@@ -1,10 +1,15 @@
 package be.alb_mar_hen.daos;
 
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import be.alb_mar_hen.models.Machine;
+import be.alb_mar_hen.utils.ObjectCreator;
 
 public class MachineDAO extends DAO<Machine>{
 
@@ -38,9 +43,26 @@ public class MachineDAO extends DAO<Machine>{
 
 	@Override
 	public List<Machine> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        String response = sendGetRequest("/machines/getAll");
+        
+        if (response.startsWith("Error:")) {
+            System.out.println("Error fetching data: " + response);
+            return new ArrayList<>();
+        }
+        
+        JSONArray jsonArray = new JSONArray(response);
+        
+        List<Machine> machines = new ArrayList<>();
+        
+        for (int i = 0; i < jsonArray.length(); i++) {
+            JSONObject jsonMachine = jsonArray.getJSONObject(i);
+            Object objectCreator = new ObjectCreator();
+			Machine machine = ((ObjectCreator) objectCreator).createMachine(jsonMachine);
+            machines.add(machine);
+        }
+        
+        return machines;
+    }
 
 	@Override
 	public List<Machine> findAll(Map<String, Object> criteria) {
